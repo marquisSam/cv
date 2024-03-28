ac9b5288807918fa9e19a1419a04bc85bab8b597
Sam <marquis.sam@gmail.com>
2024-03-28 19:10:43 -0400

    2

--- marquisSam.github.io/src/app/app.component.ts
+++ marquisSam.github.io/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = "Shiva s'gonna make it work 777";
+  title = "Shiva s'gonna make it work 777s";
 }
