05590411879624322ada9c75c08bd7488f0cbe7b
Samuel Marquis <marquis.sam@gmail.com>
2024-05-31 14:26:48 -0400

    update template

--- marquisSam.github.io/src/app/app.component.ts
+++ marquisSam.github.io/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainTemplateComponent } from './templates/main-template/main-template.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainTemplateComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'marquisSam.github.io';
--- marquisSam.github.io/src/app/templates/main-template/main-template.component.ts
+++ marquisSam.github.io/src/app/templates/main-template/main-template.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SidebarComponent } from '../../uis/sidebar/sidebar.component';
+import { WorkExperiencesComponent } from '../../uis/work-experiences/work-experiences.component';
 
 @Component({
   selector: 'cv-main-template',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SidebarComponent, WorkExperiencesComponent],
   templateUrl: './main-template.component.html',
   styleUrl: './main-template.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
