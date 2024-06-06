42f8d21e4008c988073ab75b7ef5683c6edf5eaa
Sam <marquis.sam@gmail.com>
2024-06-06 10:31:55 -0400

    sidebar update

--- marquisSam.github.io/src/app/uis/sidebar/sidebar.component.ts
+++ marquisSam.github.io/src/app/uis/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { QuickInfoComponent } from './quick-info/quick-info.component';
 
 @Component({
   selector: 'cv-sidebar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, QuickInfoComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
