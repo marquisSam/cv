6dcecfd542746b12cd4d5d71fd5305249038b746
Sam <marquis.sam@gmail.com>
2024-07-02 15:26:38 -0400

    refactor: Update file paths and imports for work experience data

--- marquisSam.github.io/src/app/app.component.ts
+++ marquisSam.github.io/src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainTemplateComponent } from './templates/main-template/main-template.component';
+interface Locale {
+  localeCode: string;
+  label: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -13,4 +17,11 @@ import { MainTemplateComponent } from './templates/main-template/main-template.c
 })
 export class AppComponent {
   title = 'marquisSam.github.io';
+
+  constructor(@Inject(LOCALE_ID) public locale: string) {}
+
+  locales: Locale[] = [
+    { localeCode: 'en', label: 'English' },
+    { localeCode: 'fr-CA', label: 'Français' },
+  ];
 }
