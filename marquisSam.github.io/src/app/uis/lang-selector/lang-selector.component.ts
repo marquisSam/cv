7653add58b5c9cb516d0068c26cf0ad435ca064b
Sam <marquis.sam@gmail.com>
2024-07-03 11:43:02 -0400

    feat: Add language selector component and update styling
    This commit adds a language selector component to the main template. It also updates the styling of the language selector component by removing the bottom padding. Additionally, the necessary imports and dependencies are added to the component file.
    
    Refactor: Add language selector component to main template
    Refactor: Update polyfills and remove i18n localization from angular.json and package.json
    Refactor: Update polyfills in angular.json to include zone.js
    Refactor: Remove i18n localization from angular.json and package.json
    Refactor: Remove i18n localization from angular.json and package.json
    Merge branch 'main' of https://github.com/marquisSam/marquissam.github.io
    Refactor: Update file paths and imports for work experience data
    Revert "Refactor: Update file paths and imports for work experience data"
    Chore: Update folder path for GitHub Pages deployment
    Chore: Update working directory for building Angular app to prod

--- marquisSam.github.io/src/app/uis/lang-selector/lang-selector.component.ts
+++ marquisSam.github.io/src/app/uis/lang-selector/lang-selector.component.ts
@@ -1,15 +1,44 @@
 import { TranslateService } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'cv-lang-selector',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './lang-selector.component.html',
   styleUrl: './lang-selector.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LangSelectorComponent {
-  constructor(private translate: TranslateService) {}
+export class LangSelectorComponent implements OnInit {
+  constructor(private translate: TranslateService) {
+    translate.addLangs(['en', 'fr']);
+    translate.setDefaultLang('fr');
+  }
+
+  lang = 'en';
+
+  ngOnInit(): void {
+    this.#selectLangFromBrowser();
+  }
+
+  updateLang(lang: string): void {
+    console.log('lang', lang);
+    this.translate.use(lang);
+  }
+
+  #selectLangFromBrowser(): void {
+    const browserLang = navigator.language.toLocaleLowerCase();
+
+    if (browserLang.match(/en/)) {
+      this.translate.use('en');
+      this.lang = 'en';
+    } else if (browserLang.match(/fr/)) {
+      this.translate.use('fr');
+      this.lang = 'fr';
+    } else {
+      this.translate.use('fr');
+      this.lang = 'fr';
+    }
+  }
 }
