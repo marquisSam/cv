0f9804fcd7a11000b5ceff197134f857cc971455
Sam <marquis.sam@gmail.com>
2024-07-09 13:32:31 -0400

    refactor: Update personal intro component with translated content
    This commit updates the personal intro component to use translated content for the introduction text. The translations are added to the language files for both English and French. This change improves the localization of the website and ensures that the correct language is displayed to the users.

--- marquisSam.github.io/src/app/uis/main-section/personal-intro/personal-intro.component.ts
+++ marquisSam.github.io/src/app/uis/main-section/personal-intro/personal-intro.component.ts
@@ -1,10 +1,11 @@
+import { TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'cv-personal-intro',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './personal-intro.component.html',
   styleUrl: './personal-intro.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
