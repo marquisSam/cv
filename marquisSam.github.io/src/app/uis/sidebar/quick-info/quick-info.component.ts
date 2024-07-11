b22b210a1750d84822cd1308ad10085812d806fd
Sam <marquis.sam@gmail.com>
2024-07-11 16:06:16 -0400

    Refactor lang selector

--- marquisSam.github.io/src/app/uis/sidebar/quick-info/quick-info.component.ts
+++ marquisSam.github.io/src/app/uis/sidebar/quick-info/quick-info.component.ts
@@ -1,3 +1,4 @@
+import { LangSelectorComponent } from './../../lang-selector/lang-selector.component';
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
@@ -5,7 +6,7 @@ import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'cv-quick-info',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, LangSelectorComponent],
   templateUrl: './quick-info.component.html',
   styleUrls: ['./quick-info.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
